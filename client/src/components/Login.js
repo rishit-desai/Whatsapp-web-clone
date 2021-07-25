b3e7869e34532dfc2290fc19700d0c9c10475c61
Rishit Desai <rishit-desai@users.noreply.github.com>
2021-07-25 15:56:06 +0530

    initial/final commit

---
+++ client/src/components/Login.js
@@ -0,0 +1,34 @@
+import React, { useRef } from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
+import { v4 as uuidv4 } from 'uuid'
+
+
+export default function Login({ onIdSubmit })
+{
+    const idRef = useRef();
+
+    function handleSubmit(e)
+    {
+        e.preventDefault();
+
+        onIdSubmit(idRef.current.value);
+    }
+
+    function createNewId()
+    {
+        onIdSubmit(uuidv4());
+    }
+
+    return (
+        <Container className="align-items-center d-flex" style={{ height: "100vh" }}>
+            <Form onSubmit={handleSubmit} className="w-100">
+                <Form.Group>
+                    <Form.Label>Enter Your ID</Form.Label>
+                    <Form.Control type="text" ref={idRef} required />
+                </Form.Group>
+                <Button type="submit" className="me-2 mt-2">Submit</Button>
+                <Button onClick={createNewId} variant="secondary" className="mt-2">Create New Id</Button>
+            </Form>
+        </Container >
+    );
+}
